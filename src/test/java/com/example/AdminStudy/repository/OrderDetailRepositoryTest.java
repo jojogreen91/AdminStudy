2687716730f4bcb4cf46dffedda657bd567536fc
jojogreen91 <iawbg13@gmail.com>
2021-07-12 16:10:43 +0900

    22.partner item order detail 테이블 테스트 - 2

---
+++ src/test/java/com/example/AdminStudy/repository/OrderDetailRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.example.AdminStudy.repository;
+
+import com.example.AdminStudy.AdminStudyApplicationTests;
+import com.example.AdminStudy.model.entity.OrderDetail;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class OrderDetailRepositoryTest extends AdminStudyApplicationTests {
+
+    @Autowired
+    OrderDetailRepository orderDetailRepository;
+
+    @Test
+    public void create () {
+
+        OrderDetail orderDetail = new OrderDetail();
+
+        orderDetail.setStatus("WAITING");
+        orderDetail.setArrivalDate(LocalDateTime.now().plusDays(2));
+        orderDetail.setQuantity(1);
+        orderDetail.setTotalPrice(BigDecimal.valueOf(900000));
+        orderDetail.setCreatedAt(LocalDateTime.now());
+        orderDetail.setCreatedBy("AdminServer");
+        orderDetail.setItemId(1L); // 어떠한 상품
+        orderDetail.setOrderGroupId(1L); // 어떠한 장바구니에
+
+        OrderDetail newOrderDetail = orderDetailRepository.save(orderDetail);
+
+        Assertions.assertNotNull(newOrderDetail);
+    }
+}
