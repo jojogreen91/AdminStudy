ce4c5018671c76a75cb799cf56b5ffa5804d07d6
jojogreen91 <iawbg13@gmail.com>
2021-07-12 13:46:29 +0900

    18.19.Entity 및 Repository 설정

---
+++ src/main/java/com/example/AdminStudy/model/entity/Item.java
@@ -0,0 +1,48 @@
+package com.example.AdminStudy.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String status;
+
+    private String name;
+
+    private String title;
+
+    private String content;
+
+    private BigDecimal price;
+
+    private String brandName;
+
+    private LocalDateTime registeredAt;
+
+    private LocalDateTime unregisteredAt;
+
+    private LocalDateTime createdAt;
+
+    private String createdBy;
+
+    private LocalDateTime updatedAt;
+
+    private String updatedBy;
+}
